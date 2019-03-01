8445e1b38b65de99b9da8eaa706405833fe705e4
Ruchi <ruchikharwar4@gmail.com>
2019-03-01 14:29:38 +0530

    update colorGenerator

--- colorGenerator/main.js
+++ colorGenerator/main.js
@@ -2,7 +2,6 @@ const header = document.querySelector('.header');
 const button = document.querySelector('.button');
 const maincontainer = document.querySelector('.main-container');
 const setcolor = document.querySelectorAll('.setcolor')
-console.log(setcolor)
 
 function get_random_color() 
 {
@@ -20,14 +19,22 @@ setcolor.forEach(item => {
 })
 
 let colorname = document.querySelectorAll('.colorname');
-console.log(colorname)
-	colorname.forEach(val => {
-		console.log(val)
+colorname.forEach(val => {
+	console.log(val)
+	const color = get_random_color();
+	val.innerHTML = color.toUpperCase().split('#').join('');
+})
+
+
+button.addEventListener('click', () => {
+  setcolor.forEach(item => {
 		const color = get_random_color();
+		item.innerHTML = color;
+	let colorname = document.querySelectorAll('.colorname');
+	colorname.forEach(val => {
 		val.innerHTML = color.toUpperCase().split('#').join('');
 	})
+		item.style.background = color;
+	})
 
-// setcolor.style.background = get_random_color();
-
-
-button.addEventListener('click', get_random_color)
+});
